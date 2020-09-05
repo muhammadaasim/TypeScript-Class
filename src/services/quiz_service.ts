75b7aea1f4722d8f2b08dd4871643082cc4db700
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2020-09-05 12:02:20 -0700

    Quiz services created

---
+++ src/services/quiz_service.ts
@@ -0,0 +1,5 @@
+export const getQuizDetails=async(totalQuestions:number, level:string){
+    const res=await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`);
+    let data=await res.json();
+    console.log(data);
+}
